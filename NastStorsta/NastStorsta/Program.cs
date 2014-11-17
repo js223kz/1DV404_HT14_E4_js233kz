8eb3e6a43d6d9195b0d629a5ee74bc4bbf5f8716
js223kz <js223kz@student.lnu.se>
2014-11-17 15:07:00 +0100

    NastSista first commit
    Find second largest number without using arrays or other similar
    datastructures

---
+++ NastStorsta/NastStorsta/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace NastStorsta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput = "";           
+            Console.Write("Mata in 10 heltal: ");
+            userInput = Console.ReadLine();
+            int maxValue = 0;
+            int currentNumber = 0;
+            int secondLargest = 0;
+            
+            string regexPattern = @"-?[\d]+";
+
+            foreach (Match match in Regex.Matches(userInput, regexPattern))
+            {
+                currentNumber = Convert.ToInt32(match.Value);
+                if (currentNumber > maxValue)
+                {
+                    secondLargest = maxValue;
+                    maxValue = currentNumber;
+                }
+                if(currentNumber < maxValue)
+                {
+                    secondLargest = currentNumber;
+                }
+                
+            }
+            Console.WriteLine("Det näststörsta talet är: " + secondLargest);   
+          
+        }
+    }
+}
