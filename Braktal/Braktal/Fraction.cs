75afd1976f2dcb61f2c8ee343b885b4d03fea5a0
js223kz <js223kz@student.lnu.se>
2014-11-18 16:54:16 +0100

    Started on Fraction

---
+++ Braktal/Braktal/Fraction.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Braktal
+{
+    class Fraction
+    {
+       private int _numerator;
+       private int _denominator;
+       //jw222hk@student.lnu.se och os222cq@student.lnu.se
+       public int Numerator{
+           get
+           {
+                return _numerator;
+           }
+           set
+           {
+               if (value == 0)
+               {
+                   throw new ArgumentException("Nämnaren kan inte vara 0");
+               }
+               _numerator = value;
+           }
+       }
+
+       public int Denominator{
+           get
+
+           {
+               return _denominator;
+           }
+           set
+           {
+               _denominator = value;
+           }
+       }
+
+       public Fraction(int denominator, int numerator){
+           Numerator = numerator;
+           Denominator = denominator;
+       }
+
+        public bool isNegative(int denominator, int numerator)
+       {
+           Denominator = denominator;
+           Numerator = numerator;
+           double result = (double)Numerator / Denominator;
+
+           if (result < 0)
+           {
+               return true;
+           }
+           else
+           {
+               return false;
+           }
+       }
+
+        public double isEqualTo(int denominator, int numerator)
+        {
+            Denominator = denominator;
+            Numerator = numerator;
+            double result = (double)Denominator / (double)Numerator;
+            return result;   
+        }
+
+        public override string ToString()
+        {
+            double result = Denominator / Numerator;
+            string stringResult = String.Format("{0}{1}", "Resultatet av bråktalet är: ", result);
+
+            return stringResult;
+        }
+    }
+}
---
+++ Braktal/Braktal/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Braktal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int denominator = 20;
+            int numerator = 10;
+
+            //Testing constructor
+            WriteHeadline("Testar konstruktorn\n");
+            Fraction constructorFraction = new Fraction(denominator, numerator);
+            Console.WriteLine("{0}{1}{2}{3}", "Täljaren är: ", denominator, "\nNämnaren är: ", numerator);
+
+            //Testing ToString method
+            WriteHeadline("\nTestar ToString\n");
+            Console.WriteLine("{0}{1}{2}{3}", "Täljaren är: ", denominator, "\nNämnaren är: ", numerator);
+            Console.WriteLine(constructorFraction.ToString());
+
+            //Testing if result is negative
+            WriteHeadline("\nTestar isNegative\n");
+            denominator = -20;
+            numerator = 10;
+            Console.WriteLine("{0}{1}{2}{3}", "Täljaren är: ", denominator, "\nNämnaren är: ", numerator);
+            Console.WriteLine("{0}{1}", "Resultatet är negativt: ", constructorFraction.isNegative(denominator, numerator));
+
+            //Testing if two results are equal or not
+            WriteHeadline("\nTestar isEqualTo\n");
+            double resultOne = constructorFraction.isEqualTo(10, 5);
+            double resultTwo = constructorFraction.isEqualTo(20, 10);
+            bool isEqual = false;
+
+            Console.WriteLine("{0}{1}", "Första resultatet är: ", resultOne);
+            Console.WriteLine("{0}{1}", "Andra resultatet är: ", resultTwo);
+
+            if (resultOne == resultTwo)
+            {
+                isEqual = true;
+                Console.WriteLine("{0}{1}", "Resultaten är lika: ", isEqual);
+            }
+            else
+            {
+                Console.WriteLine("{0}{1}", "Resultaten är lika: ", isEqual);
+            }
+        }
+
+      
+        static public void WriteHeadline(string headLine)
+        {
+            Console.BackgroundColor = ConsoleColor.DarkGreen;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(headLine);
+            Console.ResetColor();
+        }
+    }
+}
+
+            
